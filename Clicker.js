e964e93fe0e259709e24137e59a8c35f871ed579
MartinsFlorian <f.martins2005.fm@gmail.com>
2025-04-28 11:48:03 +0200

    Change value for test

--- Clicker.js
+++ Clicker.js
@@ -1,143 +1,142 @@
+let money = 100000000000;
+let nekoCoins = 100000000000;
+let autoClicker =0;
+let click = 1;
 
-let money=100000;
-let nekoCoins=1000000000000;
-let autoClickers = 0;
-let click=1;
+let clickAdd = 1;
+let clickUpgradePrice = 100;
+let clickUpgradeLevel = 0;
 
-let clickAdd=1;
-let clickUpgradePrice=100;
-let clickUpgardeLevel=0;
-
-let autoClickersMultiplier =1;
+let autoClickerMultiplier = 1;
 let autoClickerPrice = 10;
-let autoClickersUpgradePrice=250;
-let autoClickersUpgardeLevel=0;
+let autoClickerUpgradePrice = 250;
+let autoClickerUpgradeLevel = 0;
 
-let buyNekoPrice =10;
+let buyNekoPrice = 10;
 
-let convertValuePrice=250;
-let convertValueExchange=1;
-let convertUpgradePrice=500;
-let convertExchangeAdd=1;
-let convertUpgradeLevel=0;
+let converterValuePrice = 250;
+let converterValueExchange = 1;
+let converterExchangeAdd = 1;
+let converterUpgradePrice = 500;
+let converterUpgradeLevel = 0;
 
 function loadStart(){
-	updateDisplay();
-	updateTooltips();
+  updateDisplay();
+  updateTooltips();
 }
 function updateDisplay(){
-	document.getElementById("viewMoney").innerText = money;
+  document.getElementById("viewMoney").innerText = money;
   document.getElementById("viewNekoCoins").innerText = nekoCoins;
-	document.getElementById("viewAuto").innerText = autoClickers;
-	document.getElementById("viewClick").innerText = click;
-	document.getElementById("viewClickUpgrade").innerText = clickUpgardeLevel;
-	document.getElementById("viewAutoClickUpgrade").innerText = autoClickersUpgardeLevel;
-	document.getElementById("viewAutoClickMultiplier").innerText = autoClickersMultiplier;
-  document.getElementById("viewConvertUpgrade").innerText = convertUpgradeLevel;
+  document.getElementById("viewAutoClicker").innerText = autoClicker;
+  document.getElementById("viewClick").innerText = click;
+  document.getElementById("viewClickUpgrade").innerText = clickUpgradeLevel;
+  document.getElementById("viewAutoClickerUpgrade").innerText = autoClickerUpgradeLevel;
+  document.getElementById("viewAutoClickerMultiplier").innerText = autoClickerMultiplier;
+  document.getElementById("viewConverterUpgrade").innerText = converterUpgradeLevel;
 }
-function updateTooltips() {
-	document.getElementById('autoClickerPriceText').textContent = autoClickerPrice;
-	document.getElementById('buyNekoPriceText').textContent = buyNekoPrice;
-	document.getElementById('clickPriceText').textContent = clickUpgradePrice;
-	document.getElementById('clickAddText').textContent = clickAdd;
-	document.getElementById('autoClickerUpgradePriceText').textContent = autoClickersUpgradePrice;
-  document.getElementById('convertValuePriceText').textContent = convertValuePrice;
-  document.getElementById('convertValueExchangeText').textContent = convertValueExchange;
-  document.getElementById('convertUpgradePriceText').textContent = convertUpgradePrice;
-  document.getElementById('convertExchangeAddText').textContent = convertExchangeAdd;
+function updateTooltips(){
+  document.getElementById("autoClickerPriceText").textContent = autoClickerPrice;
+  document.getElementById("buyNekoPriceText").textContent = buyNekoPrice;
+  document.getElementById("clickUpgradePriceText").textContent = clickUpgradePrice;
+  document.getElementById("clickAddText").textContent = clickAdd;
+  document.getElementById("autoClickerUpgradePriceText").textContent = autoClickerUpgradePrice;
+  document.getElementById("converterValuePriceText").textContent = converterValuePrice;
+  document.getElementById("converterValueExchangeText").textContent = converterValueExchange;
+  document.getElementById("converterUpgradePriceText").textContent = converterUpgradePrice;
+  document.getElementById("converterExchangeAddText").textContent = converterExchangeAdd;
 }
 function increaseMoney(){
-	money+=click;
-	updateDisplay();
+  money += click;
+  updateDisplay();
   spawnPawPrint();
 }
 function upgradeClick(){
-	if(money >= clickUpgradePrice){
-		money-=clickUpgradePrice;
-		click+=clickAdd;
-		clickUpgardeLevel++;
-		clickUpgradePrice = Math.floor(clickUpgradePrice * 1.3);
-		
-		updateDisplay();
-		updateTooltips();
-	}else{
-		alert("Not enough money for upgrade Click!");
-	}
-}
-function convertMoneyToNekoCoins(){
-  if (money >= convertValuePrice){
-    money-=convertValuePrice;
-    nekoCoins+=convertValueExchange;
+  if(money >= clickUpgradePrice){
+    money -= clickUpgradePrice;
+    click += clickAdd;
+    clickUpgradeLevel++;
+    clickUpgradePrice = Math.floor(clickUpgradePrice * 1.3);
 
     updateDisplay();
     updateTooltips();
   }else{
-    alert("Not enough money to convert into neko coins!");
+    alert("Not enough Money for upgrade Click!")
+  }
+}
+function convertMoneyToNekoCoins(){
+  if(money >= converterValuePrice){
+    money -= converterValuePrice;
+    nekoCoins += converterValueExchange;
+
+    updateDisplay();
+    updateTooltips();
+  }else{
+    alert("Not enough Money to convert into NekoCoins!")
   }
 }
 function upgradeConverter(){
-  if(money >= convertUpgradePrice){
-    money-= convertUpgradePrice;
-    convertValueExchange+=convertExchangeAdd;
-    convertUpgradeLevel++;
-    convertUpgradePrice = Math.floor(convertUpgradePrice * 2);
+  if(money >= converterUpgradePrice){
+    money -= converterUpgradePrice;
+    converterValueExchange += converterExchangeAdd;
+    converterUpgradeLevel++;
+    converterUpgradePrice = Math.floor(converterUpgradePrice * 2);
 
     updateDisplay();
     updateTooltips();
   }else{
-    alert("Not enough money to upgrade the converter!");
+    alert("Not enough Money for upgrade converter!")
   }
 }
-function buyAutoClicker() {
-  if (money >= autoClickerPrice) {
+function buyAutoClicker(){
+  if(money >= autoClickerPrice){
     money -= autoClickerPrice;
-    autoClickers++;
+    autoClicker++;
     autoClickerPrice = Math.floor(autoClickerPrice * 1.25);
 
     updateDisplay();
     updateTooltips();
-  } else {
-    alert("Not enough money for buy an auto-clicker!");
+  }else{
+    alert("Not enough Money for buy Auto-Clicker!")
   }
 }
 setInterval(() => {
-  if (autoClickers > 0) {
-    money += autoClickers * autoClickersMultiplier;
+  if (autoClicker > 0) {
+    money += autoClicker * autoClickerMultiplier;
     updateDisplay();
   }
 }, 1000);
-function upgradeAutoClick(){
-	if (money >= autoClickersUpgradePrice) {
-    money -= autoClickersUpgradePrice;
-	autoClickersMultiplier++;
-	autoClickersUpgardeLevel++;
-    autoClickersUpgradePrice = Math.floor(autoClickersUpgradePrice * 1.3);
+function upgradeAutoClicker(){
+  if(money >= autoClickerUpgradePrice){
+    money -= autoClickerUpgradePrice;
+    autoClickerMultiplier++;
+    autoClickerUpgradeLevel++;
+    autoClickerUpgradePrice = Math.floor(autoClickerUpgradePrice * 1.3);
 
     updateDisplay();
     updateTooltips();
-  } else {
-    alert("Not enough money for upgrade Auto-Clickers!");
+  }else{
+    alert("Not enough Money for upgrade Auto-Clicker")
   }
 }
-function buyNeko() {
-  if (nekoCoins >= buyNekoPrice) {
+function buyNeko(){
+  if(nekoCoins >= buyNekoPrice){
     nekoCoins -= buyNekoPrice;
-    buyNekoPrice = Math.floor(buyNekoPrice * 1.2);
+    buyNekoPrice = Math.floor(buyNekoPrice * 1.1);
 
     updateDisplay();
     updateTooltips();
-	fetchNeko();
-  } else {
-    alert("Not enough NekoCoins to buy Neko!");
+    fetchNeko();
+  }else{
+    alert("Not enough NekoCoins for buy Neko!")
   }
 }
-function getRandomRarity() {
-    const roll = Math.random(); // 0.0 - 1.0
+function getRandomRarity(){
+  const roll = Math.random();
 
-	 if (roll < 0.7292) return 'Common';
-	 if (roll < 0.7292 + 0.2083) return 'Rare';
-	 if (roll < 0.7292 + 0.2083 + 0.0521) return 'Epic';
-	 return 'Legendary'; 
+  if(roll < 0.7292) return 'Common';
+  if(roll < 0.7292 + 0.2083) return 'Rare';
+  if(roll < 0.7292 + 0.2083 + 0.0521) return 'Epic';
+  return 'Legendary';
 }
 function fetchNeko() {
   const apiUrl = 'https://nekos.best/api/v2/neko';
@@ -175,27 +174,27 @@ function provideGiftBasedOnRarity(rarity) {
   switch (rarity) {
     case 'Common':
       money += 5;
-	  click+=3;
+	    click+=3;
       giftMessage = 'You received 5 Money and 3 Click!';
       break;
     case 'Rare':
       money += 50;
-	  click+=5;
-	  autoClickers+=3;
+	    click+=5;
+	    autoClicker+=3;
       giftMessage = 'You received 50 Money, 5 Click and 3 Auto-Clickers!';
       break;
     case 'Epic':
       money += 250;
-	  autoClickersMultiplier+=1;
-	  click+=10;
-	  autoClickers+=5;
+	    autoClickerMultiplier+=1;
+	    click+=10;
+	    autoClicker+=5;
       giftMessage = 'You received 250 Money, 10 Click, 5 Auto-Clickers and 1 Auto-Clickers Multiplier!';
       break;
     case 'Legendary':
       money += 1000;
-	  autoClickersMultiplier+=5;
-	  click+=25;
-	  autoClickers+=10;
+	    autoClickerMultiplier+=5;
+	    click+=25;
+	    autoClicker+=10;
       giftMessage = 'You received 1000 Money, 25 Click, 10 Auto-Clickers and 5 Auto-Clickers Multiplier!';
       break;
     default:
